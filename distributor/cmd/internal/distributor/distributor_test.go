e8f980f9a84630340f7f4b33740ec69cc47d6138
Martin Hutchinson <mhutchinson@gmail.com>
2023-03-20 16:11:56 +0000

    More tests for Distribute

--- distributor/cmd/internal/distributor/distributor_test.go
+++ distributor/cmd/internal/distributor/distributor_test.go
@@ -53,17 +53,17 @@ func TestGetLogs(t *testing.T) {
 		{
 			desc: "One log",
 			logs: distributor.Logs{
-				"foo": logFoo.LogInfo,
+				"FooLog": logFoo.LogInfo,
 			},
-			want: []string{"foo"},
+			want: []string{"FooLog"},
 		},
 		{
 			desc: "Two logs",
 			logs: distributor.Logs{
-				"foo": logFoo.LogInfo,
-				"bar": logBar.LogInfo,
+				"FooLog": logFoo.LogInfo,
+				"BarLog": logBar.LogInfo,
 			},
-			want: []string{"bar", "foo"},
+			want: []string{"BarLog", "FooLog"},
 		},
 	}
 	for _, tC := range testCases {
@@ -89,12 +89,12 @@ func TestGetLogs(t *testing.T) {
 
 func TestDistributeLogAndWitnessMustMatchCheckpoint(t *testing.T) {
 	ws := distributor.Witnesses{
-		"whittle": witWhittle.verifier,
-		"wattle":  witWattle.verifier,
+		"Whittle": witWhittle.verifier,
+		"Wattle":  witWattle.verifier,
 	}
 	ls := distributor.Logs{
-		"foo": logFoo.LogInfo,
-		"bar": logBar.LogInfo,
+		"FooLog": logFoo.LogInfo,
+		"BarLog": logBar.LogInfo,
 	}
 	testCases := []struct {
 		desc     string
@@ -106,40 +106,40 @@ func TestDistributeLogAndWitnessMustMatchCheckpoint(t *testing.T) {
 	}{
 		{
 			desc:     "Correct log and witness: foo and whittle",
-			reqLogID: "foo",
-			reqWitID: "whittle",
+			reqLogID: "FooLog",
+			reqWitID: "Whittle",
 			log:      logFoo,
 			wit:      witWhittle,
 			wantErr:  false,
 		},
 		{
 			desc:     "Correct log and witness: bar and wattle",
-			reqLogID: "bar",
-			reqWitID: "wattle",
+			reqLogID: "BarLog",
+			reqWitID: "Wattle",
 			log:      logBar,
 			wit:      witWattle,
 			wantErr:  false,
 		},
 		{
 			desc:     "Correct log wrong witness",
-			reqLogID: "foo",
-			reqWitID: "whittle",
+			reqLogID: "FooLog",
+			reqWitID: "Whittle",
 			log:      logFoo,
 			wit:      witWattle,
 			wantErr:  true,
 		},
 		{
 			desc:     "Wrong log correct witness",
-			reqLogID: "bar",
-			reqWitID: "whittle",
+			reqLogID: "BarLog",
+			reqWitID: "Whittle",
 			log:      logFoo,
 			wit:      witWhittle,
 			wantErr:  true,
 		},
 		{
 			desc:     "Wrong log wrong witness",
-			reqLogID: "bar",
-			reqWitID: "whittle",
+			reqLogID: "BarLog",
+			reqWitID: "Whittle",
 			log:      logFoo,
 			wit:      witWattle,
 			wantErr:  true,
@@ -158,6 +158,113 @@ func TestDistributeLogAndWitnessMustMatchCheckpoint(t *testing.T) {
 
 			logCP16 := tC.log.checkpoint(16, "16", tC.wit.signer)
 			err = d.Distribute(context.Background(), tC.reqLogID, tC.reqWitID, logCP16)
+			if (err != nil) != tC.wantErr {
+				t.Errorf("unexpected error output (wantErr: %t): %v", tC.wantErr, err)
+			}
+		})
+	}
+}
+
+func TestDistributeEvolution(t *testing.T) {
+	// The base case for this test is that a single checkpoint has already
+	// been registered for log foo, by whittle, at tree size 16, with root hash H("16").
+	ws := distributor.Witnesses{
+		"Whittle": witWhittle.verifier,
+		"Wattle":  witWattle.verifier,
+	}
+	ls := distributor.Logs{
+		"FooLog": logFoo.LogInfo,
+		"BarLog": logBar.LogInfo,
+	}
+	testCases := []struct {
+		desc     string
+		log      fakeLog
+		wit      fakeWitness
+		size     uint64
+		hashSeed string
+		wantErr  bool
+	}{
+		{
+			desc:     "whittle a bit bigger",
+			log:      logFoo,
+			wit:      witWhittle,
+			size:     18,
+			hashSeed: "18",
+			wantErr:  false,
+		},
+		{
+			desc:     "whittle smaller",
+			log:      logFoo,
+			wit:      witWhittle,
+			size:     11,
+			hashSeed: "11",
+			wantErr:  true,
+		},
+		{
+			desc:     "whittle same",
+			log:      logFoo,
+			wit:      witWhittle,
+			size:     16,
+			hashSeed: "16",
+			wantErr:  false,
+		},
+		{
+			desc:     "whittle same size but different hash",
+			log:      logFoo,
+			wit:      witWhittle,
+			size:     16,
+			hashSeed: "not 16",
+			wantErr:  true,
+		},
+		{
+			desc:     "whittle smaller different log",
+			log:      logBar,
+			wit:      witWhittle,
+			size:     11,
+			hashSeed: "11",
+			wantErr:  false,
+		},
+		{
+			desc:     "wattle smaller",
+			log:      logFoo,
+			wit:      witWattle,
+			size:     11,
+			hashSeed: "11",
+			wantErr:  false,
+		},
+		{
+			desc:     "wattle same size",
+			log:      logFoo,
+			wit:      witWattle,
+			size:     16,
+			hashSeed: "16",
+			wantErr:  false,
+		},
+		{
+			desc:     "wattle same size but different hash",
+			log:      logFoo,
+			wit:      witWattle,
+			size:     16,
+			hashSeed: "not 16",
+			wantErr:  false, // We don't check consistency with all witnesses on write
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			sqlitedb, err := sql.Open("sqlite3", ":memory:")
+			if err != nil {
+				t.Fatalf("failed to open temporary in-memory DB: %v", err)
+			}
+			d := distributor.NewDistributor(ws, ls, sqlitedb)
+			if err := d.Init(); err != nil {
+				t.Fatalf("Init(): %v", err)
+			}
+			err = d.Distribute(context.Background(), "FooLog", "Whittle", logFoo.checkpoint(16, "16", witWhittle.signer))
+			if err != nil {
+				t.Fatalf("Distribute(): %v", err)
+			}
+
+			err = d.Distribute(context.Background(), tC.log.Verifier.Name(), tC.wit.verifier.Name(), tC.log.checkpoint(tC.size, tC.hashSeed, tC.wit.signer))
 			if (err != nil) != tC.wantErr {
 				t.Errorf("unexpected error output (wantErr: %t): %v", tC.wantErr, err)
 			}
@@ -186,7 +293,7 @@ type fakeLog struct {
 	signer note.Signer
 }
 
-func (l fakeLog) checkpoint(size uint64, hashSeed string, wits ...note.Signer) []byte {
+func (l fakeLog) checkpoint(size uint64, hashSeed string, wit note.Signer) []byte {
 	hbs := sha256.Sum256([]byte(hashSeed))
 	rawCP := log.Checkpoint{
 		Origin: l.Origin,
@@ -195,7 +302,7 @@ func (l fakeLog) checkpoint(size uint64, hashSeed string, wits ...note.Signer) [
 	}.Marshal()
 	n := note.Note{}
 	n.Text = string(rawCP)
-	bs, err := note.Sign(&n, append([]note.Signer{l.signer}, wits...)...)
+	bs, err := note.Sign(&n, []note.Signer{l.signer, wit}...)
 	if err != nil {
 		panic(err)
 	}
